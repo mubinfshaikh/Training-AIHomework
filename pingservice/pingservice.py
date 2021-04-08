4f5a7d71a20909570389da2058b74050ad22f2cd
mubinfshaikh <58386068+mubinfshaikh@users.noreply.github.com>
2021-04-08 23:28:52 +0530

    Update pingservice.py

--- pingservice/pingservice.py
+++ pingservice/pingservice.py
@@ -1,1 +1,24 @@
+from flask import Flask, request, jsonify
+import requests
 
+app = Flask(__name__)
+
+@app.route('/api/v1/ping', methods = ["POST"])
+def ping():
+    response = {"code" : "", "message" : "", "data" : []}
+    try:
+        data = request.get_json()
+        request_url = str(data['url'])
+        request_response = requests.get(request_url)
+        response['code'] = 200
+        response['data'] = request_response.json()
+        response['message'] = "Success"
+        return jsonify(response)
+    except Exception as e:
+        response['code'] = 500
+        response['data'] = []
+        response['message'] = str(e)
+        return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(port=5000)
