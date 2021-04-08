54266bf6044495f7efcb657e9ad0e466bb00f402
mubinfshaikh <58386068+mubinfshaikh@users.noreply.github.com>
2021-04-08 23:29:30 +0530

    Update receiverservice.py

--- receiverservice/receiverservice.py
+++ receiverservice/receiverservice.py
@@ -1,1 +1,15 @@
+from flask import Flask, request, jsonify
+import requests
 
+app = Flask(__name__)
+
+@app.route('/api/v1/info', methods = ["GET"])
+def ping():
+    response = {"Receiver": "Cisco is the best!"}
+    try:
+        return jsonify(response)
+    except Exception as e:
+        return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(port=5001)
